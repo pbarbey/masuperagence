7d92e3a5f46bb7012351791d7c57e867dfe1d989
pbarbey <pierre.barbey3@gmail.com>
2020-09-16 18:32:35 +0200

    Modification pour la validation du formulaire et l'update des données

--- src/Controller/Admin/AdminPropertyController.php
+++ src/Controller/Admin/AdminPropertyController.php
@@ -2,11 +2,13 @@
 
 namespace App\Controller\Admin;
 use App\Entity\Property;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Repository\PropertyRepository;
 use App\Form\PropertyType;
+use Doctrine\ORM\EntityManagerInterface;
 
 class AdminPropertyController extends AbstractController
 {
@@ -14,9 +16,10 @@ class AdminPropertyController extends AbstractController
   /**
   * @var PropertyRepository
   */
-  public function __construct(PropertyRepository $repository)
+  public function __construct(PropertyRepository $repository, EntityManagerInterface $em)
   {
     $this->repository = $repository;
+    $this->em = $em;
   }
 
   /**
@@ -33,11 +36,19 @@ class AdminPropertyController extends AbstractController
   /**
   * @Route("/admin/{id}", name="admin.property.edit")
   * @param PropertyRepository $property
+  * @param Request $request
   * @return Response
   */
-  public function edit(Property $property): Response
+  public function edit(Property $property, Request $request): Response
   {
     $form = $this->createForm(PropertyType::class, $property);
+    $form->handleRequest($request);
+    if($form->isSubmitted() && $form->isValid())
+    {
+      $this->em->flush();
+      return $this->redirectToRoute('admin.property.index');
+    }
+
     return $this->render('admin/property/edit.html.twig',
   [
     'property' => $property,
